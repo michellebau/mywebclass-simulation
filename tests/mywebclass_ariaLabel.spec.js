6fc7770cc097561a1a7c28ec69f77d8b7c4222dc
Adriana Morales <89936921+amm33@users.noreply.github.com>
2023-03-22 20:21:12 -0400

    some changes

--- tests/mywebclass_ariaLabel.spec.js
+++ tests/mywebclass_ariaLabel.spec.js
@@ -1,10 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
 
 test('ARIA Label Attribute Test', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  const buttons = await page.$$('[role="button"]');
+  await page.goto('http://localhost:3000')
+  const buttons = await page.$$('[role="button"]')
   for (const button of buttons) {
-    const ariaLabel = await button.getAttribute('aria-label');
-    expect(ariaLabel).toBeTruthy();
+    const ariaLabel = await button.getAttribute('aria-label')
+    expect(ariaLabel).toBeTruthy()
   }
-});
+})
